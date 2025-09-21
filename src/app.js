b443e2ce4be808b5140beda1122d05659b3e9fc7
Ulloa-Roman <ulloaromann@gmail.com>
2025-09-21 19:15:46 -0300

    agrego funcionalidad de CORS para produccion y local

--- src/app.js
+++ src/app.js
@@ -17,7 +17,10 @@ const app = express();
 connectDB();
 
 // Middlewares
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173' }));
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
 // Rutas
